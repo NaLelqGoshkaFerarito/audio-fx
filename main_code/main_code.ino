e5b5620296104d632d35c3264dda00fa96d1299c
Vladislav Serafimov <vladiserafimov@abv.bg>
2023-07-04 13:10:33 +0300

    Added plots and sinusoid tests

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -15,19 +15,28 @@ float alpha = 0.5;
 
 
 void setup() {
+  pinMode(32, OUTPUT);
+  pinMode(33, OUTPUT);
+  pinMode(39, OUTPUT);
+  pinMode(25, INPUT);
   for(int j = 0; j < 62; ++j){
-    prev[j] = analogRead(34);
+    prev[j] = analogRead(32);
   }
 }
 
 void loop() {
+  //the modulation wave multiplier
   f_mod = map(analogRead(36), 0, 4095, 1, 5);
-  del = map(analogRead(35), 0, 4095, 1, 50);
+  //the flanger delay
+  del = map(analogRead(33), 0, 4095, 1, 50);
+  //the sinusoid coming in
   sine = analogRead(39);
-  input = analogRead(12);
+  //the input signal
+  input = analogRead(32);
   x = 1 + alpha*sine*f_mod*input;
   y = x + prev[61 - (del + (int)(range*sin(2*3.14*i*sf)*0.00011))];
-
+  analogWrite(25, y);
+  
   prev[61] = y;
   for(int j = 0; j < 60; ++j){
     prev[j] = prev[j + 1];
